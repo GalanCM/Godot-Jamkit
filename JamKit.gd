2c3065fbbb612bdcc219e9cc02358f5c9073fbb7
Galan Montgomery <galan.c.m@gmail.com>
2018-08-30 16:20:18 -0400

    Create tests for unique nodes and improve edge-case handling.

--- JamKit.gd
+++ JamKit.gd
@@ -1,23 +1,26 @@
 extends Node
 
-var _unique_nodes: Dictionary = {} setget set_unique_nodes, get_unique_nodes
+var __unique_nodes: Dictionary = {} setget __set__unique_nodes, __get__unique_nodes
 
-func set_unique_node(id: String, node: Node, allow_replacing := false):
-	if not allow_replacing and id in _unique_nodes:
-		printerr("Unique node " + id + 
-			" already exists. If you wish to allow this, try 'set_unique_node(" + 
-			id + ", " + node.name + ", true)'")
-		breakpoint
+func set_unique_node(id: String, node: Node, replace := false):
+	assert( not (replace == false and id in __unique_nodes) ) # set `replace` parameter to replace unique node
+	if replace == false and id in __unique_nodes:
+		return
 	
-	_unique_nodes[id] = Node
+	__unique_nodes[id] = node
 
 func get_unique_node(id: String) -> Node:
-	return _unique_nodes[id]
+	if not id in __unique_nodes:
+		return null
+	if id in __unique_nodes and __unique_nodes[id] == null:
+		__unique_nodes.erase(id)
+		return null
+	return __unique_nodes[id]
 
-# Setget functions for _unique_nodes. Warn users when they try to access or manipulate it. 
-func set_unique_nodes(new_unique_nodes: Dictionary):
-	printerr("JamKit: direct access to '_unique_nodes' is not recommended. Try using 'set_unique_node()' or 'replace_unique_node()' instead.")
-	_unique_nodes = new_unique_nodes
-func get_unique_nodes():
-	printerr("JamKit: direct access to '_unique_nodes' is not recommended. Try using 'get_unique_node()' instead.")
-	return _unique_nodes
+# Setget functions for __unique_nodes. Warn users when they try to access or manipulate it. 
+func __set__unique_nodes(new__unique_nodes: Dictionary):
+	printerr("JamKit: direct access to '__unique_nodes' is not recommended. Try using 'set_unique_node()' or 'replace_unique_node()' instead.")
+	__unique_nodes = new__unique_nodes
+func __get__unique_nodes():
+	printerr("JamKit: direct access to '__unique_nodes' is not recommended. Try using 'get_unique_node()' instead.")
+	return __unique_nodes
