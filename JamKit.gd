9699876a99e66be59f504c9f4dda06f9257cc4a8
Galan Montgomery <galan.c.m@gmail.com>
2018-08-30 15:35:46 -0400

    A more robust approach to handling unique nodes.
    Store them in a dictionary instead of reusing node groups.
    Means that there will only ever be one, and access can be controlled.
    For example: replacing a unique node can only be done explictly.

--- JamKit.gd
+++ JamKit.gd
@@ -1,14 +1,23 @@
 extends Node
 
+var _unique_nodes: Dictionary = {} setget set_unique_nodes, get_unique_nodes
+
+func set_unique_node(id: String, node: Node, allow_replacing := false):
+	if not allow_replacing and id in _unique_nodes:
+		printerr("Unique node " + id + 
+			" already exists. If you wish to allow this, try 'set_unique_node(" + 
+			id + ", " + node.name + ", true)'")
+		breakpoint
+	
+	_unique_nodes[id] = Node
 
 func get_unique_node(id: String) -> Node:
-	var group := get_tree().get_nodes_in_group(id)
-	if len( group ) == 0:
-		return null
-	
-	if len( group ) != 1:
-		printerr("WARNING: Unique node `" + id + "` is being used by multiple nodes:")
-		for node in group:
-			printerr("  " + node.name)
-	
-	return group[0]
+	return _unique_nodes[id]
+
+# Setget functions for _unique_nodes. Warn users when they try to access or manipulate it. 
+func set_unique_nodes(new_unique_nodes: Dictionary):
+	printerr("JamKit: direct access to '_unique_nodes' is not recommended. Try using 'set_unique_node()' or 'replace_unique_node()' instead.")
+	_unique_nodes = new_unique_nodes
+func get_unique_nodes():
+	printerr("JamKit: direct access to '_unique_nodes' is not recommended. Try using 'get_unique_node()' instead.")
+	return _unique_nodes
