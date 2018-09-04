67b517e76e3ba8f5b469e41179a7b952dbf42f76
Galan Montgomery <galan.c.m@gmail.com>
2018-09-04 14:41:37 -0400

    JamGameOver: `restart_scenes` -> `restart_modes`

--- JamGameOver.gd
+++ JamGameOver.gd
@@ -1,12 +1,12 @@
 extends TextureRect
 
-enum RestartScenes {
-	current,
-	target
+enum RestartModes {
+	current_scene,
+	target_scene
 }
 
-export(RestartScenes) var restart_scene = RestartScenes.current
-export var target_scene: PackedScene
+export(RestartModes) var restart_mode = RestartModes.current_scene
+export var restart_target: PackedScene
 
 func _ready() -> void:
 	# wait for animation to complete before accepting input
@@ -22,9 +22,9 @@ func _input(event: InputEvent) -> void:
 	
 	# restart game input (any key or action but ESC)
 	elif ( event is InputEventKey or event is InputEventAction ) and not event.is_echo():
-		if restart_scene == RestartScenes.target:
-			if target_scene != null:
-				get_tree().change_scene_to(target_scene)
+		if restart_mode == RestartModes.target_scene:
+			if restart_target != null:
+				get_tree().change_scene_to(restart_target)
 			else:
 				printerr("JamGameOver: must provide target scene when 'Restart Scene' is set to 'Target''")
 		else:
