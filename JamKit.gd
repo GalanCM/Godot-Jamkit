03cc21f24bfde2b81d0d44db592be80401629418
Galan Montgomery <galan.c.m@gmail.com>
2018-09-04 15:42:54 -0400

    Manually remove unique nodes: `remove_unique_node()`

--- JamKit.gd
+++ JamKit.gd
@@ -2,20 +2,21 @@ extends Node
 
 var __unique_nodes: Dictionary = {} setget __set__unique_nodes, __get__unique_nodes
 
-func set_unique_node(id: String, node: Node, replace := false):
+func set_unique_node(id: String, node: Node, replace := false) -> void:
 	assert( not (replace == false and id in __unique_nodes) ) # set `replace` parameter to replace unique node
-	if replace == false and id in __unique_nodes:
-		return
-	
-	__unique_nodes[id] = node
+
+	if (replace == true or not id in __unique_nodes):
+		__unique_nodes[id] = node
 
 func get_unique_node(id: String) -> Node:
 	if not id in __unique_nodes:
 		return null
 	if id in __unique_nodes and __unique_nodes[id] == null:
-		__unique_nodes.erase(id)
 		return null
 	return __unique_nodes[id]
+	
+func remove_unique_node(id: String) -> void:
+	__unique_nodes.erase(id)
 
 # Setget functions for __unique_nodes. Warn users when they try to access or manipulate it. 
 func __set__unique_nodes(new__unique_nodes: Dictionary):
