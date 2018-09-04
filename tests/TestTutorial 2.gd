44a0367adb8adaea921b5fc8ca9f795d3db99374
Galan Montgomery <galan.c.m@gmail.com>
2018-09-04 14:42:37 -0400

    TestTutorial: fixes to typing.

--- tests/TestTutorial 2.gd
+++ tests/TestTutorial 2.gd
@@ -5,12 +5,15 @@ signal next_tutorial
 func _ready() -> void:
 	hide()
 
-func _process(delta: float) -> void:
-	value = $Timer.wait_time - $Timer.time_left
+func _process(_delta: float) -> void:
+	var timer := ($Timer as Timer)
+	value = timer.wait_time - timer.time_left
 	
 func start_tutorial() -> void:
-	show()
-	$Timer.start()
+	var timer := $Timer as Timer
 	
-	yield($Timer, "timeout")
+	show()
+	timer.start()
+	
+	yield(timer, "timeout")
 	emit_signal("next_tutorial")
