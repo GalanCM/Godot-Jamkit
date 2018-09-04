e0a81d8a52702bdb2ff1e22af205f0b929e4f04f
Galan Montgomery <galan.c.m@gmail.com>
2018-09-04 15:06:14 -0400

    Automatically hide and show tutorials.

--- JamTutorial.gd
+++ JamTutorial.gd
@@ -4,6 +4,10 @@ class_name JamTutorial
 signal next_tutorial
 
 func start_tutorial() -> void:
+	for child in get_children(): # hide all children
+		if child.has_method('hide'):
+			child.hide()
+			
 	# loop through each direct child
 	for child in get_children():
 		var tutorial := child as Node
@@ -11,12 +15,18 @@ func start_tutorial() -> void:
 		# Warn if child node is missing either a `start_tutorial` method or a `next_tutorial` signal
 		assert( tutorial.has_method("start_tutorial") )
 		assert( tutorial.has_user_signal("next_tutorial") or ( tutorial.get_script() as Script ).has_script_signal("next_tutorial") )
-
+		
+		if tutorial.has_method('show'):
+			tutorial.show()
+			
 		# activate the tutorial
 		tutorial.start_tutorial()
 		
 		# wait for this tutorial to complete before going to the next one
 		yield(tutorial, "next_tutorial")
+		
+		if tutorial.has_method('hide'):
+			tutorial.hide()
 	
 	emit_signal("next_tutorial")
 	
